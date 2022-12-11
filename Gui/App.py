93f428032081b55942669fd896dddbe4cca444a5
bielowka <sbielowka@student.agh.edu.pl>
2022-12-11 15:00:29 +0100

    gui updated

--- Gui/App.py
+++ Gui/App.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import font as tkfont, HORIZONTAL
+from tkinter import font as tkfont, HORIZONTAL, ttk, LEFT, CENTER, RIGHT, X, TOP, BOTTOM
 
 
 class App(tk.Tk):
@@ -62,21 +62,31 @@ class QuestionPage(tk.Frame):
         self.page_id = page_id
         self.layer = layer
         (self.inx1, self.inx2), self.matrix = next(generator)
-        label = tk.Label(self, text=str(self.inx1) + str(self.inx2) + "" + self.matrix.title, font=controller.title_font)
+        label = tk.Label(self, text="Is "+str(self.inx1) +" better/more important than "+ str(self.inx2) + "\n in " + self.matrix.title, font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
-        self.slider = tk.Scale(self, from_= 1, to=10, orient=HORIZONTAL)
-        self.slider.pack()
+        self.choice = tk.Scale(self, from_=0, to=16, length=300, showvalue=0, orient=HORIZONTAL)
+        self.choice.set(8)
+        self.choice.pack()
+        labels=tk.Frame(self,width=300)
+        choicelabel = tk.Label(labels, text="Worse")
+        choicelabel.pack(expand=True,side=LEFT,fill="x")
+        choicelabel = tk.Label(labels, text="Similar")
+        choicelabel.pack(expand=True,side=LEFT,fill="x")
+        choicelabel = tk.Label(labels, text="Better")
+        choicelabel.pack(expand=True,side=LEFT,fill="x")
+        labels.pack(expand=True,side=TOP,fill="x")
 
         button = tk.Button(self, text="Go to the next page",
                            command=self.show_values)
-        button.pack()
+        button.pack(side=BOTTOM)
 
     def show_values(self):
+        map = [1 / 9, 1 / 8, 1 / 7, 1 / 6, 1 / 5, 1 / 4, 1 / 3, 1 / 2, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         if self.matrix.title == "criteria":
-            self.matrix.put_crit(self.inx1, self.inx2, self.slider.get())
+            self.matrix.put_crit(self.inx1, self.inx2, map[self.choice.get()])
         else:
-            self.matrix.put_obj(self.inx1, self.inx2, self.slider.get())
+            self.matrix.put_obj(self.inx1, self.inx2, map[self.choice.get()])
 
         self.controller.show_frame(self.page_id + 1)
 
@@ -89,9 +99,9 @@ class LastPage(tk.Frame):
         self.layer = layer
         label = tk.Label(self, text="This is the last page", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
+        button = tk.Button(self, text="Go to the next comparison",
                            command=self.button_action)
         button.pack()
 
-    def button_action(self): pass
-        # self.controller.show_frame(0)
+    def button_action(self):
+        self.controller.destroy()
